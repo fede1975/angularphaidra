5bf6b3e0db169cc6d6c9c0319a61ece6158d8434
fede <fede>
2017-01-31 23:04:27 +0100

    initial commit of full repository

---
+++ app.js
@@ -0,0 +1,44 @@
+angular.module('objectListApp', [])
+.factory('objectservice', function ($http) {
+  
+  var objects = []; 
+  
+    return {
+      objects: objects,
+      
+      load: function () {
+        return $http
+        .get('http://phaidradev.cab.unipd.it/api/object/o:53204/dc')
+        .success(function (data) {
+           objects.push.apply(objects, data.metadata.dc);
+        });
+        
+      },
+      loadSingle: function (dc) {
+        return $http.get('http://phaidradev.cab.unipd.it/api/object/o:53204/dc' + dc);
+        
+      }
+      
+    };
+})
+.controller('objectsController', function ($scope, objectservice) {
+  
+  $scope.objects = objectservice.objects;
+  
+  $scope.currentobject = null;
+  
+  $scope.showobject = function (dc) {
+    if (dc) {
+      objectservice.loadSingle(dc).success(function (data) {
+        $scope.currentobject = data;
+      });
+    } else {
+      $scope.currentobject = null;
+    }
+    
+  };
+  
+  objectservice.load();
+ 
+  
+});
